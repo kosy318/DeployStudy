4423bd165a4117dc957e4c6d24f09692eb18af88
kosy318 <77595685+kosy318@users.noreply.github.com>
2023-02-23 15:14:12 +0900

    Create LogListener.java

---
+++ deployTest/src/main/java/com/example/deploytest/LogListener.java
@@ -0,0 +1,11 @@
+
+
+@Service
+public class LogListener {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LogListener.class);
+
+    @KafkaListener(topics = "${kafka.topic.log}")
+    public void consume(String message) {
+        LOGGER.info("Consumed log message: {}", message);
+    }
+}
