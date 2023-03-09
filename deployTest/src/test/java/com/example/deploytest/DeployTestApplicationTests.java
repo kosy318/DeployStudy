693b8a90fbc490e99da94817d7f31ace4afe50b8
kosy318 <77595685+kosy318@users.noreply.github.com>
2023-03-09 16:02:34 +0900

    New Project Created for deploy test

---
+++ deployTest/src/test/java/com/example/deploytest/DeployTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deploytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
